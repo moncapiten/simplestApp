cdad8b872d9d747809e36e68e5af421209577722
moncapiten <moncapiten@gmail.com>
2025-07-29 02:03:53 +0200

    pdf reader website, working stage but ugly

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'pdf-guide-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  // Add your PDF files here
+  '/pdfs/sample1.pdf',
+  '/pdfs/long abstract v1.pdf',
+  '/pdfs/Documento (5).pdf'
+  // Add more PDFs as needed
+];
+
+// Install event - cache resources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
